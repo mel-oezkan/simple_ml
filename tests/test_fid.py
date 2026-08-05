74d0c2a53f7e5e81f9e8fba7c604d30690153065
mel-oezkan <melih.oezkan@stud.tu-darmstadt.de>
2026-08-05 13:24:15 +0200

    feat: added fid test comparison with clean-fid

--- tests/test_fid.py
+++ tests/test_fid.py
@@ -9,15 +9,34 @@ def test_simple_calculation():
         mu=torch.ones(fid.feature_size, dtype=torch.float64),
         prod=torch.ones(fid.feature_size, fid.feature_size, dtype=torch.float64),
         total=100,
-        mode="real"
+        mode="real",
     )
 
     fid._update_mode(
         mu=torch.ones(fid.feature_size, dtype=torch.float64),
         prod=torch.ones(fid.feature_size, fid.feature_size, dtype=torch.float64),
         total=100,
-        mode="fake"
+        mode="fake",
     )
 
     dist = fid.frechet_distance()
     print(dist)
+
+
+def test_self_distance():
+    fid = FID()
+
+    fid_distance = fid.frechet_distance_from_folder(
+        folder_real="/content/imagenet-debug",
+        folder_fake="/content/imagenet-debug",
+    )
+    # -5.4788788006021605e-05
+
+    from cleanfid import fid
+    cleanfid_score = fid.compute_fid(
+        fdir1="/content/imagenet-debug", 
+        fdir2="/content/imagenet-debug"
+    )
+    # -5.943278975450994e-05
+
+    assert torch.allclose(fid_distance, cleanfid_score, atol=(cleanfid_score * 0.05))
