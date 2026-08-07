135e9dc81044a72d448d3c8da7ebf90940ae538e
mel-oezkan <melih.oezkan@stud.tu-darmstadt.de>
2026-08-08 00:06:13 +0200

    feat: added kid test and fix naming taken from colab

--- tests/test_fid.py
+++ tests/test_fid.py
@@ -23,20 +23,51 @@ def test_simple_calculation():
     print(dist)
 
 
-def test_self_distance():
+def test_fid_distance():
     fid = FID()
 
     fid_distance = fid.frechet_distance_from_folder(
-        folder_real="/content/imagenet-debug",
-        folder_fake="/content/imagenet-debug",
+        folder_real="data/imagenet-debug",
+        folder_fake="data/imagenet-debug",
     )
     # -5.4788788006021605e-05
 
     from cleanfid import fid
     cleanfid_score = fid.compute_fid(
-        fdir1="/content/imagenet-debug", 
-        fdir2="/content/imagenet-debug"
+        fdir1="data/imagenet-debug", 
+        fdir2="data/imagenet-debug"
     )
     # -5.943278975450994e-05
 
-    assert torch.allclose(fid_distance, cleanfid_score, atol=(cleanfid_score * 0.05))
+    expected = torch.tensor(
+        cleanfid_score,
+        dtype=fid_distance.dtype,
+        device=fid_distance.device,
+    ) 
+
+    assert torch.allclose(fid_distance, expected, rtol=0.05)
+
+
+def test_kid_distance():
+    fid = FID()
+
+    kid_distance = fid.kid_distance_from_folder(
+        folder_real="data/imagenet-debug",
+        folder_fake="data/imagenet-debug",
+    )
+    # -0.0164
+
+    from cleanfid import fid
+    cleanfid_score = fid.compute_kid(
+        fdir1="data/imagenet-debug", 
+        fdir2="data/imagenet-debug"
+    )
+    # -0.01790899597108364
+    
+    expected = torch.tensor(
+        cleanfid_score,
+        dtype=kid_distance.dtype,
+        device=kid_distance.device,
+    ) 
+
+    assert torch.allclose(kid_distance, expected, rtol=0.05)
