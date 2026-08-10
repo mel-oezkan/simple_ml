502e13a96f601988ca8edce87df80743ae13124c
mel-oezkan <melih.oezkan@stud.tu-darmstadt.de>
2026-08-10 23:08:35 +0200

    helper script to run the classification eval (for modal)

---
+++ scripts/eval/__init__.py
@@ -0,0 +1,3 @@
+def eval_model(cfg):
+    """Main eval code that computes the FID and conditioned accuracy of a model."""
+    pass
---
+++ scripts/eval/eval_accuracy.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from uuid import UUID
+
+from vit.evaluation.cond_acc import classifier_evaluation
+from vit.data.transforms import TransformFashionMNIST
+
+
+def eval_classification_accuracy(cfg, runs_path: Path, generation_id: str) -> dict:
+    """Main eval code that computes the FID and conditioned accuracy of a model.
+
+    :param generatio_id: The ID of the generation run to evaluate.
+    """
+    # ? no seed setting is needed since no rng is used
+
+    # determine the run_id from the checkpoint path
+    configured_checkpoint = cfg.eval.checkpoint_path
+    run_id = str(UUID(configured_checkpoint.parent.name))
+
+    # define the storage path for the eval run
+    eval_path = runs_path / run_id
+    generated_image_path = eval_path / generation_id
+
+    acc, cond_acc, confusion_mat = classifier_evaluation(
+        cfg,
+        classifier_path=Path(cfg.eval.classifier),
+        generated_image_path=generated_image_path,
+        transform=TransformFashionMNIST.eval,
+    )
+
+    return {
+        "accuracy": acc,
+        "conditioned_accuracy": cond_acc,
+        "confusion_matrix": confusion_mat,
+    }
