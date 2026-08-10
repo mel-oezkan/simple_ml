1ffc2bf44c6a2eee251918514ca7612b29c2a892
mel-oezkan <melih.oezkan@stud.tu-darmstadt.de>
2026-08-10 23:00:55 +0200

    fix: incorrectly loading the model

--- vit/evaluation/cond_acc.py
+++ vit/evaluation/cond_acc.py
@@ -1,19 +1,20 @@
-from omegaconf import DictConfig
-import torch
-
 from pathlib import Path
 
+import numpy as np
+import torch
+from omegaconf import DictConfig
 from torch.utils.data import DataLoader
+from torchvision import transforms
 from torchvision.datasets import ImageFolder
 
 from vit.evaluation.fid import load_backbone_processor
-from torchvision import transforms
+from vit.models.classifier import CNNClassifier
 
 
 def acc(pred, label):
     if pred.ndim > label.ndim:
         pred = pred.argmax(dim=-1)
-        
+
     return (pred == label).float().mean()
 
 
@@ -21,7 +22,7 @@ def conditioned_acc(cfg, labels: torch.Tensor, predictions: torch.Tensor):
     device = predictions.device
     confusion_mat = torch.zeros(
         (cfg.data.n_classes, cfg.data.n_classes),
-        dtype=torch.lang,
+        dtype=torch.long,
         device=device
     )
 
@@ -40,22 +41,22 @@ def conditioned_acc(cfg, labels: torch.Tensor, predictions: torch.Tensor):
 
 
 def classifier_evaluation(
-    cfg: DictConfig, classifier_path: Path, generated_image_path: Path
-):
+    cfg: DictConfig, 
+    classifier_path: Path, 
+    generated_image_path: Path,
+    transform: transforms.Compose = None
+) -> tuple[float, dict[int, float], np.ndarray]:
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    model = torch.load(classifier_path)
+    # init the classifier and then load the model
+    model = CNNClassifier(cfg.data.n_classes)
+    model.load_state_dict(torch.load(classifier_path, map_location=device))
     model = model.to(device)
     model.eval()
 
     ds_generated = ImageFolder(
         generated_image_path,
-        transform=transforms.Compose(
-            [
-                transforms.ToTensor(),
-                transforms.Normalize((0.5,), (0.5,)),
-            ]
-        ),
+        transform=transform,
     )
 
     data_loader_generated = DataLoader(
@@ -86,7 +87,7 @@ def classifier_evaluation(
             correct / per_class_total if per_class_total > 0 else "N/A"
         )
 
-    return total_correct / len(ds_generated), per_class_acc
+    return total_correct / len(ds_generated), per_class_acc, confusion_mat.cpu().numpy()
 
 
 if __name__ == "__main__":
