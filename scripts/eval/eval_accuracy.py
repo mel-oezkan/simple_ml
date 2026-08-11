467ef1ed286b50524a93e14242e5d38af4fe7bc3
mel-oezkan <melih.oezkan@stud.tu-darmstadt.de>
2026-08-11 02:04:20 +0200

    use current dir instead of dynamically locating run_id + toGRayscale

--- scripts/eval/eval_accuracy.py
+++ scripts/eval/eval_accuracy.py
@@ -1,29 +1,32 @@
 from pathlib import Path
 
-from vit.evaluation.cond_acc import classifier_evaluation
+from torchvision import transforms
+
 from vit.data.transforms import TransformFashionMNIST
+from vit.evaluation.cond_acc import classifier_evaluation
 
 
-def eval_classification_accuracy(cfg, runs_path: Path, generation_id: str) -> dict:
+def eval_classification_accuracy(cfg, runs_id_path: Path, generation_id: str) -> dict:
     """Main eval code that computes the FID and conditioned accuracy of a model.
 
     :param generatio_id: The ID of the generation run to evaluate.
     """
     # ? no seed setting is needed since no rng is used
 
-    # determine the run_id from the checkpoint path
-    configured_checkpoint = Path(cfg.eval.checkpoint_path)
-    run_id = configured_checkpoint.parent.name
-
     # define the storage path for the eval run
-    eval_path = runs_path / run_id
+    eval_path = runs_id_path 
     generated_image_path = eval_path / generation_id
 
     acc, cond_acc, confusion_mat = classifier_evaluation(
         cfg,
         classifier_path=Path(cfg.eval.classifier),
         generated_image_path=generated_image_path,
-        transform=TransformFashionMNIST.eval,
+        transform=transforms.Compose(
+            [
+                transforms.Grayscale(num_output_channels=1),
+                TransformFashionMNIST.eval,
+            ]
+        ),
     )
 
     return {
